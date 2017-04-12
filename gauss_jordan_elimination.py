a3e7c747bd897e14e5a838c7ac81bf42453ba567
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-04-12 16:07:29 -0500

    Adding non-functional Gauss Jordan.

---
+++ gauss_jordan_elimination.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from numba import cuda
+import numpy as np
+import time, csv, sys
+
+@cuda.jit
+def gauss_jordan(A, b, size):
+  idx = cuda.threadIdx.x
+  idy = cuda.threadIdx.y
+  if idx < size and idy < size:
+    for i in range(1, size):
+      if (idy+1) < size:
+        var = (-1) * (A[(i-1)*size+(i-1)]/A[(i+idy)*size+(i-1)])
+        A[(i+idy)*size+idx] = A[(i-1)*size+idx] + ((var) * A[(i+idy)*size+idx])
+      cuda.syncthreads()
+    b[idy * (size+1) + idx] = A[idy*size + idx]
+
+def gauss_jordan2(A, size):
+  idx = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+  idy = cuda.blockIdx.y * cuda.blockDim.y + cuda.threadIdx.y
+  size += 1
+
+  for i in range(0, size):
+    if idx < size and idy < size:
+      if idx > i:
+        pivot = A[idx*size+i]/A[i*size+i]
+        if idy >= i:
+          A[idx*size+idy]-= A[i*size+idy] * pivot
+      cuda.syncthreads()
+
+def gauss_jordan1(A, size):
+  idx = cuda.threadIdx.x
+  idy = cuda.threadIdx.y
+  size += 1
+
+  cuda.syncthreads()
+
+  if idx < size and idy < size:
+    for j in range(0, size):
+      pivot = A[j*size + j]
+      A[idx*size + j] /= pivot
+      cuda.syncthreads()
+      if idy != j:
+        A[idx*size+idy] -= A[j*size+idy] * A[idx*size+j]
+      cuda.syncthreads()
+
+def main(argv):
+  if len(argv) != 3:
+    print("Usage: python3.6 gauss_jordan_elimination.py <A_matrix> <b_matrix>")
+    sys.exit()
+
+  A_name = argv[1]
+  b_name = argv[2]
+
+  with open(A_name) as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    matrix = list(reader)
+    A_matrix = np.array(matrix).astype("float64")
+    A = A_matrix.flatten()
+  with open(b_name) as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    matrix = list(reader)
+    b_matrix = np.array(matrix).astype("float64")
+    b = b_matrix.flatten()
+  """
+  b = b_matrix.reshape(len(b_matrix), 1)
+  A = np.hstack((A_matrix, b))
+  A = A.flatten()
+  """
+  rows      = len(b)
+  columns   = len(b)
+  tpb       = 32
+  matrix_size = rows * columns
+
+  gpu_A = cuda.to_device(A)
+  gpu_b = cuda.to_device(b)
+  bpg = matrix_size + (tpb - 1) // tpb
+
+  gauss_jordan[bpg, tpb](gpu_A, gpu_b, rows)
+
+  #A = gpu_A.copy_to_host()
+  #print(A.reshape(rows, (columns+1))[:, columns])
+  #print(A.reshape(rows, (columns+1)))
+
+  b = gpu_b.copy_to_host()
+  A = gpu_A.copy_to_host()
+  print(b)
+  print(A)
+
+if __name__ == "__main__":
+  main(sys.argv)
