f4d0d7854f30d6acd6f9450cec8bbb1824502e84
Diego Ocampo <jocamp18@eafit.edu.co>
2017-04-12 16:18:54 -0500

    GUI with sparse matrix implementation

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -6,58 +6,59 @@ from pprint import pprint
 
 matrix_length = 20
 density = 0.2
+class SparseMatrix():
+	def create_sparse_matrix(self, filename):
+		val = 0 
+		pos = 0 
+		aux_pos = 0
+		matrix = []
+		pointerB = []
+		pointerE = []
+		columns = []
+		values = []
 
-def create_sparse_matrix(filename):
-	val = 0 
-	pos = 0 
-	aux_pos = 0
-	matrix = []
-	pointerB = []
-	pointerE = []
-	columns = []
-	values = []
+		pointerB.append(pos)
+		for i in range(0, matrix_length):
+			row = []
+			if(pos != aux_pos):
+				pointerB.append(pos)
+			aux_pos = pos
+			for j in range(0, matrix_length):
+				probability = random.random()
+				if probability < density:
+					pos += 1
+					val = random.randint(1,10)
+					values.append(val)
+					columns.append(j)
+				else:
+					val = 0
+				row.append(val)
+			if(pos != aux_pos):
+				pointerE.append(pos)
+			matrix.append(row)
+			
+		data = {"values": values, "columns": columns, "pointerB":pointerB, "pointerE":pointerE}
+		data_json = json.dumps(data)
+		file = open(filename, 'w')
+		file.write(data_json)
+		file.close()
+		print("Matrix")
+		print(matrix)
 
-	pointerB.append(pos)
-	for i in range(0, matrix_length):
-		row = []
-		if(pos != aux_pos):
-			pointerB.append(pos)
-		aux_pos = pos
-		for j in range(0, matrix_length):
-			probability = random.random()
-			if probability < density:
-				pos += 1
-				val = random.randint(1,10)
-				values.append(val)
-				columns.append(j)
-			else:
-				val = 0
-			row.append(val)
-		if(pos != aux_pos):
-			pointerE.append(pos)
-		matrix.append(row)
-		
-	data = {"values": values, "columns": columns, "pointerB":pointerB, "pointerE":pointerE}
-	data_json = json.dumps(data)
-	file = open(filename, 'w')
-	file.write(data_json)
-	file.close()
-	print("Matrix")
-	print(matrix)
-
-def load_sparse_matrix(filename):
-	with open(filename) as data_file:    
-		data = json.load(data_file)
-	values = data["values"]
-	print("JSON")
-	print(values)
+	def load_sparse_matrix(self, filename):
+		with open(filename) as data_file:    
+			data = json.load(data_file)
+		values = data["values"]
+		print("JSON")
+		print(values)
 
 def main(argv):
 	if len(argv) != 2:
 		print("Unsage: ./program filename")
 		sys.exit()
-	create_sparse_matrix(argv[1])
-	load_sparse_matrix(argv[1])
+	sparseMatrix = SparseMatrix()
+	sparseMatrix.create_sparse_matrix(argv[1])
+	sparseMatrix.load_sparse_matrix(argv[1])
 
 if __name__ == '__main__':
 	main(sys.argv)
