f1087c48136a5bdd5bf7f37cc1e0b40f7b1366ff
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-05-28 13:47:06 -0500

    Enabling file reading by blocks.

--- project/jacobi/read_rows.py
+++ project/jacobi/read_rows.py
@@ -7,16 +7,21 @@ import time, csv, sys
 from jacobi_parallel_chunks import JacobiParallel
 
 def main(argv):
-  b_file = open("../b.txt").read().split('\n')[:-1]
-  A_file = open("../a.txt").read().split('\n')[:-1]
+  a_name = argv[1]
+  b_name = argv[2]
+  rows_to_read = int(argv[3])
+  matrix_size = int(argv[4])
+  niter = int(argv[5])
+
+  b_file = open(b_name).read().split('\n')[:-1]
+  A_file = open(a_name).read().split('\n')[:-1]
   jp = JacobiParallel()
 
-  rows_to_read = int(argv[1])
-  matrix_size = int(argv[2])
-  niter = int(argv[3])
   x_current = np.zeros(matrix_size, dtype=np.float64)
   count = 0
+  print("Comenzando jacobi ...")
   while count < niter:
+    print("Iteracion: ", count)
     x_next = []
     A = []
     b = []
@@ -40,12 +45,12 @@ def main(argv):
       count2 += 1
     x = jp.start(np.array(A).flatten(), np.array(b),\
                  x_current, (count2 - len(A)))
-    #print(x)
     x_next = np.append(x_next, x)
     x_current = np.array(x_next).flatten()
+    print("x_current: \n", x_current[:10])
     count += 1
 
-  print(x_next)
+  print(x_next[:10])
 
 if __name__ == "__main__":
   main(sys.argv)
