85db877314e8b2945ba3791c4faa30591a585190
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2017-06-04 12:54:33 -0500

    Tunning code to avoid unnecesary computations.

--- project/block_operations/jacobi_parallel_chunks.py
+++ project/block_operations/jacobi_parallel_chunks.py
@@ -6,7 +6,7 @@
     Authors: Tomás Felipe Llano Ríos,
              Juan Diego Ocampo García,
              Johan Sebastián Yepes Ríos
-    Date last modified: 03-June-2017
+    Date last modified: 04-June-2017
     Python Version: 3.6.0
 """
 from numba import cuda
@@ -19,6 +19,20 @@ class JacobiParallel:
     @cuda.jit('void(float64[:], float64[:], float64[:], float64[:],'\
               'int32, int32, int32, float32)', target='gpu', nopython=True)
     def jacobi(A, b, x_current, x_next, rows, cols, first_row_block, rel):
+        """Performs jacobi for every thread in matrix A boundaries.
+
+        Key arguments:
+        A -- Matrix extracted from the coefficient matrix A.
+        b -- Vector extracted from Linearly independent vector b.
+        x_current -- Current answer's approximation.
+        x_next -- vector in which to store new answer.
+        rows -- Number of rows read (i.e. number of rows in the block).
+        cols -- Number of columns from the original matrix.
+        first_row_block -- Integer indicating the first row of the block by
+        using an index from the coefficient matrix A (i.e. What is the
+        correspondence between the first block's row and A).
+        rel -- Relaxation coefficient.
+        """
         idx = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
         current_row = first_row_block + idx
         if idx < rows:
@@ -49,9 +63,10 @@ class JacobiParallel:
             col += 1
 
         A = A.flatten()
+        A_size = len(A)
         tpb = 32
-        bpg = len(A) + (tpb - 1) // tpb
-        cols = len(A) // rows
+        bpg = A_size + (tpb - 1) // tpb
+        cols = A_size // rows
         x_next = np.zeros(rows, dtype=np.float64)
         gpu_A = cuda.to_device(A)
         gpu_b = cuda.to_device(b)
